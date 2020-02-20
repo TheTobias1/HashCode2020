e9a1a8cf45c733a4d61b08957fef654ddc47e46d
diegocalanzone <diegocalanzone@gmail.com>
2020-02-20 18:30:23 +0000

    File parsing x

---
+++ parsing.py
@@ -0,0 +1,60 @@
+
+class Library:
+
+    def __init__(self, books, max_b, s_days):
+        self.books = books
+        self.max_books = max_b
+        self.signup_days = s_days
+
+class Book:
+    def __init__(self, score):
+        self.score = score
+
+
+
+#####################################
+
+days = 0
+books = []
+
+filepath = 'b_read_on.txt'
+
+with open(filepath) as fp:
+   
+    # General definition
+    line = fp.readline()
+    arr = line.split()
+    arr = [ int(x) for x in arr ]
+
+    days = arr[2]
+
+    # Book scores
+    line = fp.readline()
+    books = line.split()
+
+    books = [ int(x) for x in books ]
+
+    while line:
+
+        # Line 1
+        line = fp.readline()
+        values = line.split()
+        values = [ int(x) for x in values ]
+
+        if len(values) == 0:
+            break
+
+        lib = Library([], values[2], values[1])
+        
+        # Line 2
+        line = fp.readline()
+        book_ids = line.split()
+        book_ids = [ int(x) for x in book_ids ]
+
+        for book_id in book_ids:
+            lib.books.append(book_id)
+
+        if not line:
+            break
+        
+
